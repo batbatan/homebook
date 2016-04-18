35e1f3bde14797c3690cdae3b93146b71d81ec41
batbatan <batbatan@users.noreply.github.com>
2016-04-18 18:05:53 +0300

    Update jquery.ui.datepicker-bg.js

--- Scripts/jquery.ui.datepicker-bg.js
+++ Scripts/jquery.ui.datepicker-bg.js
@@ -1,5 +1,5 @@
 /* Bulgarian initialisation for the jQuery UI date picker plugin. */
-/* Written by Stoyan Kyosev (http://svest.org). */
+/* Created by NachoSoft */
 jQuery(function($){
     $.datepicker.regional['bg'] = {
         closeText: 'затвори',
